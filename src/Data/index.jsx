9540063bba9cc878316e8290c74f0fdcbe34d25c
Ikumi Mine <71612923+IkumiMine@users.noreply.github.com>
2023-10-10 15:47:13 -0400

    updated url

--- src/Data/index.jsx
+++ src/Data/index.jsx
@@ -28,7 +28,7 @@ const ProjectsDetails = () => {
             "description": "This application allows a user to search their flight information which includes airports, airlines, and schedule by flight number. If the flight's location information is available, the real-time location of the airplane will be marked on the Google Map.",
             "tech": "HTML, CSS/Bootstrap, PHP, JavaScript, Aviationstack API, Google Maps API",
             "image": Flight,
-            "webUrl": "http://ikumi.lovestoblog.com/APIproject/",
+            "webUrl": "https://ikumi-mine.000webhostapp.com/flight-information/",
             "gitUrl": "https://github.com/IkumiMine/Flight-Information"
         },
         {
@@ -39,7 +39,7 @@ const ProjectsDetails = () => {
             "description": "This is a group project to build a dynamic and data-driven web application with a booking system for the doctor's office. I created the FAQ page and admin dashboard as a developer and designer.",
             "tech": "HTML, CSS/Bootstrap, JavaScripot, PHP, MySQL, Fullcalendar, Swiftmailer",
             "image": QCHC,
-            "webUrl": "http://ikumi.lovestoblog.com/project-querycrew/",
+            "webUrl": "https://ikumi-mine.000webhostapp.com/querycrew/",
             "gitUrl": "https://github.com/IkumiMine/project-querycrew"
         },
         {
@@ -50,7 +50,7 @@ const ProjectsDetails = () => {
             "description": "This is the ticket support system application. A logged-in user as staff can see a list of all tickets and detailed each ticket, and reply to them. A logged-in user as client can create a new ticket, see a list of their tickets and detailed each ticket, and reply to the message from staff.",
             "tech": "HTML, CSS/Bootstrap, PHP, XML",
             "image": SupportTicket,
-            "webUrl": "http://ikumi.lovestoblog.com/assignment2/",
+            "webUrl": "https://ikumi-mine.000webhostapp.com/support-ticket-system/",
             "gitUrl": "https://github.com/IkumiMine/Support-Ticket-System"
         },
       
